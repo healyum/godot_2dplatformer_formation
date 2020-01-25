00f19d5db8b52fba34d906532eee58ea83227f5b
benjamin <benjamin@mail.com>
2020-01-25 14:41:22 +0100

    feat(player): add gravity to the player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,12 +4,15 @@ const SPEED = 700
 # On indique la direction vers le haut 
 const FLOOR_NORMAL = Vector2(0, -1)
 
+const GRAVITY = 2000
+
 # Pour le déplacement du personnage (direction)
 var motion = Vector2()
 
 # Update / tourne en boucle
 func _physics_process(delta):
 	walk()
+	gravity(delta)
 	move_and_slide(motion, FLOOR_NORMAL)
 	
 func walk():
@@ -26,3 +29,8 @@ func walk():
 		$AnimatedSprite.play("idle")
 		motion.x = 0
 		
+func gravity(delta):
+	if is_on_floor(): # si on touche le sol on ne tombe pas
+		motion.y = 0
+	else: # sinon appliqué gravité
+		motion.y += GRAVITY * delta
