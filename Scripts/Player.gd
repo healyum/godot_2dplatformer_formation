75ee5f1dfffb727a679d8c39f5add02c7f85ac87
benjamin <benjamin@mail.com>
2020-01-25 14:16:59 +0100

    refactor(player): move player script

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,14 +9,20 @@ var motion = Vector2()
 
 # Update / tourne en boucle
 func _physics_process(delta):
+	walk()
+	move_and_slide(motion, FLOOR_NORMAL)
+	
+func walk():
 	# Si on appuie sur la touche droite ou D
 	if Input.is_action_pressed("ui_right"):
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = false # on regarde à droite
 		motion.x = SPEED
-		print("right")
 	elif Input.is_action_pressed("ui_left"):
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = true # on regarde à gauche
 		motion.x = -SPEED
-		print("left")		
 	else:
+		$AnimatedSprite.play("idle")
 		motion.x = 0
 		
-	move_and_slide(motion, FLOOR_NORMAL)
