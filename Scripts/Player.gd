7378b61054ede47a9e82a3f6f51422982d0d2ddd
benjamin <benjamin@mail.com>
2020-01-26 14:45:39 +0100

    feat(sounds): add jewel + jump sounds

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -13,6 +13,14 @@ var motion = Vector2()
 
 var coins = 0
 
+var coin_sound
+
+var jump_sound
+
+func _ready():
+	coin_sound = load("res://Sounds/coin1.wav")
+	jump_sound = load("res://Sounds/jump_01.wav")
+
 # Update / tourne en boucle
 func _physics_process(delta):
 	walk()
@@ -43,5 +51,14 @@ func gravity(delta):
 func jump():
 	if is_on_floor() and Input.is_action_pressed("jump"):
 		motion.y = JUMP_FORCE
+		play_jump()
 	if motion.y < 0:
 		$AnimatedSprite.play("jump")
+		
+func play_jump():
+	$AudioStreamPlayer2D.stream = jump_sound
+	$AudioStreamPlayer2D.play()
+		
+func play_coin():
+	$AudioStreamPlayer2D.stream = coin_sound
+	$AudioStreamPlayer2D.play()
--- Scripts/redjewel.gd
+++ Scripts/redjewel.gd
@@ -5,6 +5,7 @@ onready var PlayerScript = get_tree().get_root().find_node("Player", true, false
 onready var GUIScript = get_tree().get_root().find_node("GUI", true, false)
 
 func _on_Area2D_body_entered(body):
+	PlayerScript.play_coin()
 	PlayerScript.coins += 1
 	GUIScript.change_val(PlayerScript.coins)
 	queue_free() # fait disparaitre le sprit
